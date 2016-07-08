59d26b2e1ad416000efab7d15581254d419a9540
lucianoluconi <lucianoluconi@gmail.com>
2016-07-08 15:14:00 -0300

    home controller

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -31,12 +31,15 @@ app.controller("homeCtrl", function ($rootScope, $route, $scope, $http, $locatio
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 	$('.carousel').carousel({
       interval: 5000
     });
 });
 =======
+=======
+>>>>>>> parent of f7f88d5... ajustes
 =======
 >>>>>>> parent of f7f88d5... ajustes
 =======
@@ -67,6 +70,10 @@ app.directive('disableAnimate', ['$animate', function ($animate) {
     };
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+} ]);
+>>>>>>> parent of f7f88d5... ajustes
+=======
 } ]);
 >>>>>>> parent of f7f88d5... ajustes
 =======
